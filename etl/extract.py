fe990f776bb2cbdb21272bf8998bd169d5891aeb
Nini_Kheladze <116943557+FeliniSy@users.noreply.github.com>
2025-10-05 00:27:31 +0400

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # data for MSFT
-url1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey==IGDO97FQIAAOJMV8'
+url1 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey==ninisKey'
 r1 = requests.get(url1)
 data1 = r1.json()
 
@@ -12,7 +12,7 @@ with open("extracted_data\MSFT_2025-10-04.json", "w") as f1:
     json.dump(data1, f1, indent=4)
 
 # data for GOOG
-url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&apikey==IGDO97FQIAAOJMV8'
+url2 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&apikey==ninisKey'
 r2 = requests.get(url2)
 data2 = r2.json()
 
@@ -23,7 +23,7 @@ with open("extracted_data\GOOG_2025-10-04.json", "w") as f2:
 
 
 # data for AAPL
-url3 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey==IGDO97FQIAAOJMV8'
+url3 = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey==ninisKey'
 r3 = requests.get(url3)
 data3 = r3.json()
 
